e746b1a1e67a884c7b66b55d4aa4867b5bcd6e23
ryalser <129397957+ryalser@users.noreply.github.com>
2025-08-28 14:37:56 +0300

    Update OneThread.java

--- noBags/src/main/java/practice_9/task_2/OneThread.java
+++ noBags/src/main/java/practice_9/task_2/OneThread.java
@@ -2,19 +2,23 @@ package practice_9.task_2;
 
 public class OneThread implements Runnable{
 
-    @Override
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("A");
-        }
+        @Override
+        public void run() {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("A");
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException("Поток прерван");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException("Поток прерван");
+                }
+            }
         }
     }
-}
+
+
+// По итогам проверки перенес блок try-catch в сам цикл.
+ 
 
 
 
