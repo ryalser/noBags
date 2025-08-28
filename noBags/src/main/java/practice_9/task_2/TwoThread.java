45382176031f97cabbfb7ddb0a59cd6a49435c03
ryalser <129397957+ryalser@users.noreply.github.com>
2025-08-28 14:38:28 +0300

    Update TwoThread.java

--- noBags/src/main/java/practice_9/task_2/TwoThread.java
+++ noBags/src/main/java/practice_9/task_2/TwoThread.java
@@ -2,16 +2,19 @@ package practice_9.task_2;
 
 public class TwoThread implements Runnable {
 
-    @Override
+   @Override
     public void run() {
         for (int i = 1; i <= 5; i++) {
             System.out.println("B");
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Поток прерван");
+            }
         }
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException("Поток прерван");
-        }
     }
 }
+
+// По итогам проверки перенес блок try-catch в сам цикл.
